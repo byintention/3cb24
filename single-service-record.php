4021b83b4dc7d19eda6b5aad7de68beb0baf1b21
Nick Hayward <info@fcswebsites.co.uk>
2025-02-12 22:57:55 +0000

    add edit button back in

--- single-service-record.php
+++ single-service-record.php
@@ -29,12 +29,11 @@ if (
 		<div class="post white serviceRecord" id="post-<?php the_ID(); ?>">
 			<div class="padded" style="padding-bottom:0;">
 				<ul class="breadcrumb has-small-font-size">
-					<li><a href="/">Home</a></li>
-					<li><a href="/roster/">Roster</a></li>
+					<li><a href="<?php echo esc_url( home_url() ); ?>">Home</a></li>
+					<li><a href="<?php echo esc_url( home_url() ); ?>/roster/">Roster</a></li>
 					<li><?php the_title(); ?></li>
 				</ul>
 			</div>
-
 			<div class="entry padded">
 				<div class="container">
 				<?php 
@@ -123,6 +122,21 @@ if (
 					}
 					?>
 					</table>
+					
+					<div class="tcb_user_edit_options">
+					<?php
+					$roles = wp_get_current_user()->roles;
+					if (in_array( 'training_admin', $roles)) {
+						echo '<br><a href="'. home_url() .'/edit-training-record/?id=' . $userId . '" class="button button-secondary">Edit Training Record</a><br>';
+					}
+					if (in_array( 'commendation_admin', $roles)) {
+						echo '<br><a href="'. home_url() .'/edit-ribbons/?id=' . $userId . '" class="button button-secondary">Edit Commendations</a><br>';
+					}
+					if (in_array( 'administrator', $roles)) {
+						echo '<br><a href="'. home_url() .'/edit-service-record/?id=' . $userId . '" class="button button-secondary">Edit Service Record</a><br>';
+					}
+					?>
+					</div>
 				</div>
 				<div class="tcb_user_training five columns">
 					<?php
