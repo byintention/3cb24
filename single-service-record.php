29e1c5966277cd27999566122782a685176b7236
Nick Hayward <info@fcswebsites.co.uk>
2025-02-13 00:10:10 +0000

    service record single WPCS

--- single-service-record.php
+++ single-service-record.php
@@ -17,15 +17,14 @@
 
 <?php
 if (
-	( in_array( 'training_admin', wp_get_current_user()->roles)) || 
-	( in_array( 'recruit_admin', wp_get_current_user()->roles)) || 
-	( in_array( 'administrator', wp_get_current_user()->roles)) ) {
-		?>	
-	<?php
+	( in_array( 'training_admin', wp_get_current_user()->roles ) ) ||
+	( in_array( 'recruit_admin', wp_get_current_user()->roles ) ) ||
+	( in_array( 'administrator', wp_get_current_user()->roles ) ) ) {
+
 	if ( have_posts() ) :
-		while (have_posts()) :
+		while ( have_posts() ) :
 			the_post();
-				?>
+			?>
 		<div class="post white serviceRecord" id="post-<?php the_ID(); ?>">
 			<div class="padded" style="padding-bottom:0;">
 				<ul class="breadcrumb has-small-font-size">
@@ -36,78 +35,79 @@ if (
 			</div>
 			<div class="entry padded">
 				<div class="container">
-				<?php 
-				$isAdmin = in_array( 'administrator', wp_get_current_user()->roles);
-				$userId = get_field( 'user_id' );
-				if ($userId != "") {
-					$user = get_user_by( 'id', $userId );
+				<?php
+				$is_admin = in_array( 'administrator', wp_get_current_user()->roles );
+				$user_id  = get_field( 'user_id' );
+				if ( $user_id != "" ) {
+					$user = get_user_by( 'id', $user_id );
 				} else {
-					$user = wp_get_current_user();
-					$userId = $user->ID;
+					$user    = wp_get_current_user();
+					$user_id = $user->ID;
 				}
-				$displayName = $user->get( 'display_name' );
-				$userProfile = 'user_' . $userId;
-				$postIdField = 'post_id'; 
-				$postId = get_field( $postIdField, $userProfile );
+				$display_name    = $user->get( 'display_name' );
+				$user_profile    = 'user_' . $user_id;
+				$record_id_field = 'record_id';
+				$record_id       = get_field( $record_id_field, $user_profile );
 				?>
 				<div class="tcb_user_info five columns">
 					<h3>User info</h3>
 					<?php
-					// Rank
-					$path = plugins_url() . '/tcb-roster/images/ranks/';
-					$rank = get_field( 'rank', $postId );
-					if ( !$rank )
+					// Rank.
+					$rank_path = plugins_url() . '/tcb-roster/images/ranks/';
+					$rank      = get_field( 'rank', $record_id );
+					if ( !$rank ) {
 						return $return;
+					}
 					?>
-					<p><img src="<?php echo $path . $rank['value']; ?>.gif" title="<?php echo $rank['label']; ?>" style="width:144px;height:240px;"></p>
+					<p><img src="<?php echo $rank_path . $rank['value']; ?>.gif" title="<?php echo $rank['label']; ?>" style="width:144px;height:240px;"></p>
 					<table>
 						<tr><td>Rank</td><td><?php echo $rank['label']; ?></td></tr>
 					<?php
-					// Location
-					$location = get_field( 'user-location', $userProfile );
+					// Location.
+					$location = get_field( 'user-location', $user_profile );
 					if ($location) {
 						?>
 						<tr><td>Location</td><td><?php echo $location; ?></td></tr>
 						<?php
 					}
-					// Steam ID
-					$steamID = get_field( 'steam_id', $postId );
-					if ($isAdmin && $steamID) {
+					// Steam ID.
+					$steam_id = get_field( 'steam_id', $record_id );
+					if ( $is_admin && $steam_id ) {
 						?>
-						<tr><td>Steam ID</td><td><?php echo $steamID; ?></td></tr>
+						<tr><td>Steam ID</td><td><?php echo $steam_id; ?></td></tr>
 						<?php
 					}
-					// Discord ID
-					$discordID = get_field( 'discord_id', $userProfile );
-					if ($isAdmin && $discordID) {
+					// Discord ID.
+					$discord_id = get_field( 'discord_id', $user_profile );
+					if ( $is_admin && $discord_id ) {
 						?>
-						<tr><td>Discord ID</td><td><?php echo $discordID; ?></td></tr>
+						<tr><td>Discord ID</td><td><?php echo $discord_id; ?></td></tr>
 						<?php
 					}
-					// Email
+					// Email.
 					$email = $user->user_email;
-					if ($isAdmin && $email) {
+					if ( $is_admin && $email ) {
 						?>
 						<tr><td>Email</td><td><?php echo $email; ?></td></tr>
 						<?php
 					}
-					// Dates
+					// Dates.
 					if ( $rank['value'] == 'Rct' ) {
-						$dateStr = get_field( 'attestation_date', $postId );
-						$date = DateTime::createFromFormat( 'd/m/Y', $dateStr );
-						if ($date) {
-							$now = new DateTime('now');
+						$date_str = get_field( 'attestation_date', $record_id );
+						$date     = DateTime::createFromFormat( 'd/m/Y', $date_str );
+						if ( $date ) {
+							$now      = new DateTime( 'now' );
 							$interval = $date->diff( $now );
 							?>
-							<tr><td>Attestation</td><td><?php echo date_format($date, 'd-m-Y'); ?></td></tr>
-							<tr><td>Length of recruit period: <?php echo $interval->format('%y year(s), %m month(s), %d day(s)'); ?></td></tr>
+							<tr><td>Attestation</td><td><?php echo date_format( $date, 'd-m-Y' ); ?></td></tr>
+							<tr><td>Length of recruit period: <?php echo $interval->format( '%y year(s), %m month(s), %d day(s)' ); ?></td></tr>
 							<?php
 						}
 					} else {
-						$dateStr = get_field( 'passing_out_date', $postId );
-						$date = DateTime::createFromFormat( 'd/m/Y', $dateStr );
-						if ($date) {
-							$now = new DateTime('now');
+						$date_str = get_field( 'passing_out_date', $record_id );
+						$date     = DateTime::createFromFormat( 'd/m/Y', $date_str );
+						if ( $date ) {
+							$now      = new DateTime( 'now' );
 							$interval = $date->diff( $now );
 							?>
 							<tr><td>Passing out</td><td><?php echo date_format( $date, 'd-m-Y' ); ?></td></tr>
@@ -115,8 +115,9 @@ if (
 							<?php
 						}
 					}
-					// LOA
-					if ( (get_field( 'loa', $postId ) == 1 ) && ( $rank['value'] != "Res" ) ) { ?>
+					// LOA.
+					if ( ( get_field( 'loa', $record_id ) == 1 ) && ( $rank['value'] != 'Res' ) ) {
+						?>
 						<tr><td>Approved LOA</td></tr>
 						<?php
 					}
@@ -126,27 +127,28 @@ if (
 					<div class="tcb_user_edit_options">
 					<?php
 					$roles = wp_get_current_user()->roles;
-					if (in_array( 'training_admin', $roles)) {
-						echo '<br><a href="'. home_url() .'/edit-training-record/?id=' . $userId . '" class="button button-secondary">Edit Training Record</a><br>';
+					if ( in_array( 'training_admin', $roles ) ) {
+						echo '<a href="' . home_url() . '/edit-training-record/?id=' . $user_id . '" class="button button-secondary">Edit Training Record</a>';
 					}
-					if (in_array( 'commendation_admin', $roles)) {
-						echo '<br><a href="'. home_url() .'/edit-ribbons/?id=' . $userId . '" class="button button-secondary">Edit Commendations</a><br>';
+					if ( in_array( 'commendation_admin', $roles ) ) {
+						echo '<a href="' . home_url() . '/edit-ribbons/?id=' . $user_id . '" class="button button-secondary">Edit Commendations</a>';
 					}
-					if (in_array( 'administrator', $roles)) {
-						echo '<br><a href="'. home_url() .'/edit-service-record/?id=' . $userId . '" class="button button-secondary">Edit Service Record</a><br>';
+					if ( in_array( 'administrator', $roles ) ) {
+						echo '<a href="' . home_url() . '/edit-service-record/?id=' . $user_id . '" class="button button-secondary">Edit Service Record</a>';
 					}
 					?>
 					</div>
 				</div>
 				<div class="tcb_user_training five columns">
 					<?php
-					// Duties
-					$listOfDuties = get_field( 'duties', $postId );
-					if ( $listOfDuties ) {
+					// Duties.
+					$list_of_duties = get_field( 'duties', $record_id );
+					if ( $list_of_duties ) {
 						?>
 						<h3>Administrative duties</h3><ul>
 						<?php
-						foreach ( $listOfDuties as $duty ) { ?>
+						foreach ( $list_of_duties as $duty ) {
+							?>
 							<li><?php echo $duty['label']; ?></li>
 							<?php
 						}
@@ -161,7 +163,7 @@ if (
 					if ( $list_of_courses ) {
 						?>
 						<ul>
-							<?php 
+							<?php
 							foreach ( $list_of_courses as $course ) {
 								echo '<li>' . $course['label'] . '</li>';
 							}
@@ -175,30 +177,33 @@ if (
 				<div class="tcb_user_ribbons two columns">
 					<h3>Commendations</h3>
 					<?php
-					$path = plugins_url() . '/tcb-roster/images/ribbons/';
-					$dateStr = get_field( 'passing_out_date' );
-					$date = DateTime::createFromFormat('d/m/Y', $dateStr);
-					if ($date) {
-						$now = new DateTime('now');
-						$interval = $date->diff($now);
+					$ribbon_path = plugins_url() . '/tcb-roster/images/ribbons/';
+					$date_str    = get_field( 'passing_out_date' );
+					$date        = DateTime::createFromFormat( 'd/m/Y', $date_str );
+					if ( $date ) {
+						$now          = new DateTime( 'now' );
+						$interval     = $date->diff( $now );
 						$served_years = $interval->y;
-						if ($served_years > 0) { ?>
-							<p><img src="<?php echo $path . 'service-' . $served_years;?>.png" title="Service award, year <?php echo  $served_years; ?>" width="350" height="94"></p>
-						<?php
+						if ( $served_years > 0 ) {
+							?>
+							<p><img src="<?php echo $ribbon_path . 'service-' . $served_years; ?>.png" title="Service award, year <?php echo $served_years; ?>" width="350" height="94"></p>
+							<?php
 						}
 					}
 					$list_of_ribbons = get_field( 'operational_awards' );
 					if ( $list_of_ribbons ) {
-						foreach ( $list_of_ribbons as $ribbon ) { ?>
-							<p><img src="<?php echo $path . $ribbon['value']; ?>.png" title="<?php echo $ribbon['label']; ?>" width="350" height="94"></p>
-						<?php
+						foreach ( $list_of_ribbons as $ribbon ) {
+							?>
+							<p><img src="<?php echo $ribbon_path . $ribbon['value']; ?>.png" title="<?php echo $ribbon['label']; ?>" width="350" height="94"></p>
+							<?php
 						}
 					}
 					$list_of_ribbons = get_field( 'community_awards' );
 					if ( $list_of_ribbons ) {
-						foreach ( $list_of_ribbons as $ribbon ) { ?>
-							<p><img src="<?php echo $path . $ribbon['value']; ?>.png" title="<?php echo $ribbon['label']; ?>" width="350" height="94"></p>
-						<?php
+						foreach ( $list_of_ribbons as $ribbon ) {
+							?>
+							<p><img src="<?php echo $ribbon_path . $ribbon['value']; ?>.png" title="<?php echo $ribbon['label']; ?>" width="350" height="94"></p>
+							<?php
 						}
 					}
 					?>
@@ -209,12 +214,12 @@ if (
 			<?php
 			comments_template();
 			?>
-		<?php 
+			<?php
 			endwhile;
 		endif;
-		?>
+	?>
 	</div>
-<?php
+	<?php
 } else {
 	echo '<p class="negative">Not authorised</p>';
 }
