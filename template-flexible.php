cbfa0b8ba8c90a3f867acb8ec2ac43cfe95cfa33
Nick Hayward <info@fcswebsites.co.uk>
2025-03-18 22:09:37 +0000

    CSS refine, UI date picker fix

--- template-flexible.php
+++ template-flexible.php
@@ -5,10 +5,25 @@
 	@package 3cb24
  */
 
-?>
-<?php acf_form_head(); ?>
-<?php get_header(); ?>
+acf_form_head();
+get_header(); ?>
 	<div class="wrap clear">
+		<div id="content">
+			<div class="container textPage">
+				<div class="twelve columns">
+				<?php
+				if ( have_posts() ) {
+					while ( have_posts() ) {
+						the_post();
+						the_content();
+					}
+				}
+				?>
+				</div>
+			</div>
+		</div>
+		
+		<div id="success-message" style="display:none;"></div>
 	<?php get_template_part( 'includes/flexible-content' ); ?>
 	</div>
 <?php get_footer(); ?>
